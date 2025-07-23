60560df8f644bf2195d8166d3ddd989d36f3a031
STRG-Z <110018726+PyrexHitech25@users.noreply.github.com>
2025-07-23 10:43:48 +0200

    Update Rock, Paper, Scissor.py
    Version 2.0

--- Rock, Paper, Scissor.py
+++ Rock, Paper, Scissor.py
@@ -1,50 +1,41 @@
 import random
+from user_input_jn import yes_or_no
+
 
 def greetings():
     print("Welcome to my Rock, Paper, Scissor - Game, hope you enjoy! :)\n")
-    game()
+    game_loop()
+
 
 def comp_choice():
     return random.choice(['Rock', 'Paper', 'Scissor'])
 
-def game():
-    print("You can choose between 'Rock', 'Paper' and 'Scissor'!")
+
+def game_loop():
     while True:
-        player_input = str(input("\n").strip().title())
-        if player_input in ('Rock', 'Paper', 'Scissor'):
-            break
-        else:
-            print("Choose between 'Rock', 'Paper' and 'Scissor'!")
-    computer_choice = comp_choice()
-    try:
+        print("You can choose between 'Rock', 'Paper' and 'Scissor'!")
+        while True:
+            player_input = str(input("\n").strip().title())
+            if player_input in ('Rock', 'Paper', 'Scissor'):
+                break
+            else:
+                print("Choose between 'Rock', 'Paper' and 'Scissor'!")
+
+        computer_choice = comp_choice()
+
         if player_input == computer_choice:
-            print("Its a tie!\n")
+            print("It's a tie!\n")
         elif (
-            (player_input == "Rock" and computer_choice == "Scissor") or
-            (player_input == 'Paper' and computer_choice == "Rock") or
-            (player_input == "Scissor" and computer_choice == "Paper")
+                (player_input == "Rock" and computer_choice == "Scissor") or
+                (player_input == 'Paper' and computer_choice == "Rock") or
+                (player_input == "Scissor" and computer_choice == "Paper")
         ):
-            print(f"You win! :), i got {computer_choice}\n")
+            print(f"You win! :), I got {computer_choice}\n")
         else:
-            print(f"You lose! :(, i got {computer_choice}\n")
-        play_again()
-    except ValueError:
-        print("Please choose between 'Rock', Paper' or 'Scissor'!\n")
+            print(f"You lose! :(, I got {computer_choice}\n")
 
-def play_again():
-    print("Do you wanna play again? j/n\n")
-    while True:
-        again = str(input("").strip().lower())
-        if again == "j":
-            game()
-        elif again == "n":
-            print("Thanks fpr playing :)\n")
+        print("Do you wanna play again? y/n\n")
+        if not yes_or_no():
+            print("Thanks for playing!\n")
             break
-        else:
-            print("Just type 'j' or 'n'\n")
 greetings()
-
-
-
-
-
